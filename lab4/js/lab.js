00766461f8dee5ef566cd3fa65c09fa93627f090
rhianneweir <riweir@ucsc.edu>
2024-10-17 20:24:51 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -6,6 +6,15 @@
 
 // Functions
 
+//Pseudocode
+  // Print a 3x3 grid
+  // Print 3x3 grid in those boxes
+  // Print numbers that do not change
+  // Allow user input for numbers 1-9
+  // Check boxes, rows, and columns for errors
+  // If error print “error” or error message
+
+
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
