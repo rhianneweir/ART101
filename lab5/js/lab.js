9dadf5965ca27ea880798c93acfe55095c6cc240
rhianneweir <riweir@ucsc.edu>
2024-10-21 18:06:01 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,16 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Rhianne Weir
+// Date: 10-21-24
 
 // Constants
+make = "Ford"
+model = "Corolla"
+color = "black"
+year = 2020
 
-// Functions
+// Calculations
+currentYear = 2024
+age = currentYear - year
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -14,6 +20,12 @@ function myFunction(param1, param2) {
 
 function main() {
   console.log("Main function started.");
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + " years<br>");
+  
   // the code that makes everything happen
 }
 
