a02719bcf868c55635d075192f34469762447b29
rhianneweir <riweir@ucsc.edu>
2024-10-28 21:27:18 -0700

    dsfghjk

---
+++ lab7/js/lab.js
@@ -0,0 +1,26 @@
+// index.js - FUNCTIONS
+// Author: Rhianne Weir
+// Date: 10-28-24
+
+// Constants
+
+// Functions
+
+function nameSort() {
+  let userName = prompt("Enter your name here:");
+  let sort = userName.split("");
+  let sortedName = sort.join();
+  return sortedName;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+ let a = nameSort(); 
+ 
+  document.writeln("Your sorted name: <pre>", 
+    JSON.stringify(a, null, '\t'), "</pre>");
+}
+
+// let's get this party started
+main();
