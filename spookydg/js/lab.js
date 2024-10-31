b5ed6827842279b36e64cdae87ffc7768064a5a3
rhianneweir <riweir@ucsc.edu>
2024-10-31 11:40:47 -0700

    spooky dg

---
+++ spookydg/js/lab.js
@@ -0,0 +1,29 @@
+// index.js - FUNCTIONS
+// Author: Rhianne Weir
+// Date: 10-28-24
+
+// Constants
+
+// Functions
+
+function spookify() {
+  let userName = prompt("Enter your name here to get your spooky persona: ");
+  if(userName.toLower() == "andres"){
+    return userName;
+  }
+  var nameArray = ["scary ", "spooky ", "creepy "];
+
+  return nameArray[Math.random(0,2)] + userName ;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+ let a = spookify(); 
+ 
+  document.writeln("Your sorted name: <pre>", 
+    JSON.stringify(a, null, '\t'), "</pre>");
+}
+
+// let's get this party started
+main();
