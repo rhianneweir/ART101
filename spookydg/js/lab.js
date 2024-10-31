626d453434a89bd93d07aa195ec846b11a050bfc
rhianneweir <riweir@ucsc.edu>
2024-10-31 12:34:38 -0700

    test

--- spookydg/js/lab.js
+++ spookydg/js/lab.js
@@ -6,24 +6,41 @@
 
 // Functions
 
-function spookify() {
-  let userName = prompt("Enter your name here to get your spooky persona: ");
-  if(userName.toLower() == "andres"){
-    return userName;
-  }
-  var nameArray = ["scary ", "spooky ", "creepy "];
-  let please = nameArray[Math.floor(Math.random()*4)];
-  return please + userName;
+// function spookify() {
+//   let userName = prompt("Enter your name here to get your spooky persona: ");
+//   if(userName.toLower() == "andres"){
+//     return userName;
+//   }
+//   var nameArray = ["scary ", "spooky ", "creepy "];
+//   let please = nameArray[Math.floor(Math.random()* 4)];
+//   return please + userName;
+// }
+
+// function main() {
+//   console.log("Main function started.");
+//   // the code that makes everything happen
+//  let a = spookify(); 
+ 
+//   document.writeln("Your spooky name: <pre>", 
+//     JSON.stringify(a, null, '\t'), "</pre>");
+// }
+
+function nameSort() {
+  let userName = prompt("Enter your name here:");
+  var sort = userName.toLowerCase().split('').sort().join('');
+  return sort;
 }
 
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
- let a = spookify(); 
+ let a = nameSort(); 
  
   document.writeln("Your sorted name: <pre>", 
     JSON.stringify(a, null, '\t'), "</pre>");
 }
+
+
 
 // let's get this party started
 main();
