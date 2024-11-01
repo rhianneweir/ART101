bae9ca26bc8fabb4da5333697e86e5f178959551
rhianneweir <riweir@ucsc.edu>
2024-10-31 20:04:44 -0700

    frree me its halqoween

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -9,14 +9,12 @@
 function addingStuff(x) {
   return x+x ;
 }
-function squared(x){
-  return x*x;
-}
+
 function main() {
   console.log("Main function started.");
   //let res = addingStuff(2);
-  let xx = squared(3);
-  const nums = [2, 5, 10, 15];
+ 
+  var nums = [2, 5, 10, 15];
  let res = nums.map(addingStuff);
   document.writeln("Result: <pre>", 
     JSON.stringify(res, null, '\t'), "</pre>");
