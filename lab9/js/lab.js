15d8733655b6cc1c8994c612a984763bdd8f0e96
rhianneweir <riweir@ucsc.edu>
2024-11-11 22:11:55 -0800

    idk what i did

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -10,10 +10,10 @@ function idkyet(){
 }
 
 function main() {
-  $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+  
 // add a click listener to the challenge button
-$("#button-challenge").click(function(){
-  // now add (or subtract) the "special" class to the section
+$("#specialify").click(function(){
+
   $("#challenge").toggleClass("special");
 });
 
