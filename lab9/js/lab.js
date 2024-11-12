41082e5d3d43c39f647c93e3efce208e60ddf4c3
rhianneweir <riweir@ucsc.edu>
2024-11-11 22:34:51 -0800

    pleaseeee

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -10,12 +10,26 @@ function idkyet(){
 }
 
 function main() {
-  
+// add button to challenge section
+$("#Challenge").append("<button id='button-challenge'>Make Special</button>");
 // add a click listener to the challenge button
-$("#specialify").click(function(){
+$("#button-challenge").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $("#Challenge").toggleClass("special");
+});  
+$("#Problems").append("<button id='button-challenge'>Make Special</button>");
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $("#Problems").toggleClass("special");
+}); 
+$("#Results").append("<button id='button-challenge'>Make Special</button>");
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $("#Results").toggleClass("special");
+});  
 
-  $("#challenge").toggleClass("special");
-});
 
 }
 
