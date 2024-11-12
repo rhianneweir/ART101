6fcd433790113aa76adeeadcb3e19618ada5f8cd
rhianneweir <riweir@ucsc.edu>
2024-11-11 22:46:00 -0800

    why is there 3

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -5,30 +5,34 @@
 // Constants
 
 // Functions
-function idkyet(){
 
-}
 
 function main() {
 // add button to challenge section
 $("#Challenge").append("<button id='button-challenge'>Make Special</button>");
+
+  // add a click listener to the challenge button
+  $("#button-challenge").click(function(){
+
+     // now add (or subtract) the "special" class to the section
+     $("#Challenge").toggleClass("special");
+});  
+$("#Problems").append("<button id='button-challenge'>Make Special</button>");
+
 // add a click listener to the challenge button
 $("#button-challenge").click(function(){
+
   // now add (or subtract) the "special" class to the section
-  $("#Challenge").toggleClass("special");
+  $("#Problems").toggleClass("special");
+}); 
+$("#Results").append("<button id='button-challenge'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+  
+  // now add (or subtract) the "special" class to the section
+  $("#Results").toggleClass("special");
 });  
-// $("#Problems").append("<button id='button-challenge'>Make Special</button>");
-// // add a click listener to the challenge button
-// $("#button-challenge").click(function(){
-//   // now add (or subtract) the "special" class to the section
-//   $("#Problems").toggleClass("special");
-// }); 
-// $("#Results").append("<button id='button-challenge'>Make Special</button>");
-// // add a click listener to the challenge button
-// $("#button-challenge").click(function(){
-//   // now add (or subtract) the "special" class to the section
-//   $("#Results").toggleClass("special");
-// });  
 
 
 }
