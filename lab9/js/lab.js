359c986eb8472c5935986a55c46d536c6a8cf714
rhianneweir <riweir@ucsc.edu>
2024-11-11 22:37:07 -0800

    shkfjb

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -17,18 +17,18 @@ $("#button-challenge").click(function(){
   // now add (or subtract) the "special" class to the section
   $("#Challenge").toggleClass("special");
 });  
-$("#Problems").append("<button id='button-challenge'>Make Special</button>");
-// add a click listener to the challenge button
-$("#button-challenge").click(function(){
-  // now add (or subtract) the "special" class to the section
-  $("#Problems").toggleClass("special");
-}); 
-$("#Results").append("<button id='button-challenge'>Make Special</button>");
-// add a click listener to the challenge button
-$("#button-challenge").click(function(){
-  // now add (or subtract) the "special" class to the section
-  $("#Results").toggleClass("special");
-});  
+// $("#Problems").append("<button id='button-challenge'>Make Special</button>");
+// // add a click listener to the challenge button
+// $("#button-challenge").click(function(){
+//   // now add (or subtract) the "special" class to the section
+//   $("#Problems").toggleClass("special");
+// }); 
+// $("#Results").append("<button id='button-challenge'>Make Special</button>");
+// // add a click listener to the challenge button
+// $("#button-challenge").click(function(){
+//   // now add (or subtract) the "special" class to the section
+//   $("#Results").toggleClass("special");
+// });  
 
 
 }
