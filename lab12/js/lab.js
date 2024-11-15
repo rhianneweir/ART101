205143e468486f33ebb1b3a8297f0fabe1fe0c6f
rhianneweir <riweir@ucsc.edu>
2024-11-14 20:54:26 -0800

    fart

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -10,16 +10,8 @@
 
 // Functions
 
-function generateRandomText() {
-  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-  const min = 3;
-  const max = 100;
-  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-  // Get a random starting index to slice the Lorem Ipsum text
-  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-  // Generate the random Lorem Ipsum-like text
-  return text.slice(randStart, randStart + randLen);
-}
+
+
 
 function main() {
   const newText = generateRandomText();
