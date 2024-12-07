469ebe847ad30115123d233a7eb71ad8393e4e12
rhianneweir <riweir@ucsc.edu>
2024-12-06 19:42:39 -0800

    OHFUCL

---
+++ lab15/js/lab.js
@@ -0,0 +1,47 @@
+// index.js - CONDITIONALS
+// Author: Rhianne Weir
+// Date: 10-28-24
+/*
+   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+
+   Requirements: jQuery must be loaded for this script to work.
+*/
+// Constants
+
+// Functions
+
+$.ajax({
+  // The URL for the request (from the api docs)
+  url: "https://yourapiendpoint.com/",
+  // The data to send (will be converted to a query string)
+  data: { 
+          // here is where any data required by the api 
+          //   goes (check the api docs)
+          id: 123,
+          api_key: "blahblahblah",
+        },
+  // Whether this is a POST or GET request
+  type: "GET",
+  // The type of data we expect back
+  dataType : "json",
+  // What do we do when the api call is successful
+  //   all the action goes in here
+  success: function(data) {
+      // do stuff
+      console.log(data);
+  },
+  // What we do if the api call fails
+  error: function (jqXHR, textStatus, errorThrown) { 
+      // do stuff
+      console.log("Error:", textStatus, errorThrown);
+  }
+})
+
+
+
+
+
+  
+
+
+
