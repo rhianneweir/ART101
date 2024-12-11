098eaed03ecd6e86a470ed8b153ef65b8f8d379c
rhianneweir <riweir@ucsc.edu>
2024-12-10 16:23:45 -0800

    kjdcn

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,11 +1,8 @@
 // index.js - CONDITIONALS
 // Author: Rhianne Weir
-// Date: 10-28-24
-/*
-   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+// Date: 10-28-24 /* lab.js - This simple JavaScript/jQuery script appends new elements to an output div
 
-   Requirements: jQuery must be loaded for this script to work.
-*/
+Requirements: jQuery must be loaded for this script to work.
 // Constants
 
 // Functions
@@ -16,79 +13,51 @@ function idk() {
     // The data to send (will be converted to a query string)
     data: {
       count: 248,
-      next: "https://pokeapi.co/api/v2/ability/?limit=20&offset=20",
-      previous: null,
-      results: [
+      next:"https://pokeapi.co/api/v2/ability/?limit=20&offset=20",
+      previous:null,
+      results:[
         {
-          name: "stench",
+          name:"stench",
           url:"https://pokeapi.co/api/v2/ability/1/"
         }
       ]
-    },
-    
       
-    
-
-      
+  
 
   
   // Whether this is a POST or GET request
-  type:"GET",
   
-      
     
 
       
 
   
-  // The type of data we expect back
-  dataType:"json",
-  
-
-      
-
-  
-  // What do we do when the api call is successful
-  
-        
-    
-
-      
-  
-
-  
-  
-        
-    
-
-      
-  
-  
-    
-  
-  
-  
-    
-  
-    
-  
-    
-  
-
-  
-
-  
-
-  
-
-  
-
-
-})
-
-$("#activate").click(function(){
-$("#output").append('<div class="text"><p>' + idk() + '</p></div>')
-
- } );
- 
 }
+  
+, type:"GET"
+    
+
+      
+
+  
+  
+  
+
+, 
+dataType:
+"json"
+    
+  
+
+
+,
+
+
+success:function(data) 
+    
+  
+ {} },
+
+$("#activate").click(function () {
+  
+}))}
